3cb21895adb4027d4cdec451700cc58e7ed88366
burcuozbek <kburcuozbek@gmail.com>
2022-07-06 21:04:10 +0300

    sticky notes

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container d-flex justify-content-center flex-column">
+    <h3 class="d-flex justify-content-center align-items-center mt-5 mb-3">
+      Sticky Notes
+    </h3>
+
+    <NoteBoard />
+  </div>
+</template>
+<script>
+import NoteBoard from "@/components/NoteBoard.vue";
+
+export default {
+  data() {
+    return { notes: ["note"] };
+  },
+  components: {
+    NoteBoard,
+  },
+};
+</script>
---
+++ src/components/NoteBoard.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="btn btn-danger align-self-center mb-4" @click="addNote()">
+    Add a New Note
+  </button>
+  <div class="board d-flex justify-content-center flex-row flex-wrap">
+    <div class="note d-flex flex-column" v-for="note in notes" :key="note">
+      <button
+        class="btn-sm btn-danger remove align-self-end"
+        @click="removeNote(index)"
+      >
+        x
+      </button>
+      <textarea
+        class="bg-transparent align-self-center"
+        cols="30"
+        rows="10"
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notes: ["note"],
+    };
+  },
+  methods: {
+    removeNote(index) {
+      console.log("silindi");
+      this.notes.splice(index, 1);
+    },
+    addNote() {
+      this.notes.push("note");
+      console.log("eklendi");
+    },
+
+  },
+};
+</script>
